c3517ddd39cdda29abaa365c995eefc619e064a4
Sydne Hanks <sydnehanks@gmail.com>
2020-11-24 20:03:24 -0700

    ldfkjf

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,19 @@
 class Contact < ApplicationRecord
+has_many :notes
+has_one :address
+# Associations 
+  # describing the relationship between models
+   # child model
+  # belongs_to
+   # parent model
+  # has_many
+  # has_one 
+  # has_many :through
+  # has_one :through
+  # dependent 
+  has_many :notes, dependent: :destroy
+  # has_one :note 
+  has_one :address, dependent: :destroy
+
+
 end
